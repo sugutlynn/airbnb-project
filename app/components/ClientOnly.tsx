75839fa985768421aa8a170d3bd8d237e8822e74
sugutlynn <lynn.sugut@getsote.com>
2023-07-25 23:03:42 +0300

    airbnb-project

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React, { useEffect, useState } from "react";
+
+interface ClientOnlyProps {
+    children: React.ReactNode;
+}
+
+const ClientOnly = (props: ClientOnlyProps) => {
+    const { children } = props;
+    const [hasMounted, setHasMounted] = useState(false);
+
+    useEffect(() => {
+        setHasMounted(true)
+    }, []);
+
+    if (!hasMounted) {
+        return null;
+    }
+
+    return(
+        <>
+          {children}
+        </>
+    );
+}
+
+export default ClientOnly;
