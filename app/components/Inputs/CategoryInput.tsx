7a6c33c892f041da525a4190f2d8fa9e33794bf0
sugutlynn <sugutlynn@gmail.com>
2023-08-28 23:23:25 +0300

    Move Input to folder & add category input

---
+++ app/components/Inputs/CategoryInput.tsx
@@ -0,0 +1,9 @@
+const CategoryInput = () => {
+    return ( 
+        <div>
+             
+        </div>
+     );
+}
+ 
+export default CategoryInput;
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -8,7 +8,7 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
-import Input from '../Input';
+import Input from '../Inputs/Input';
 import toast from 'react-hot-toast';
 import Button from '../Button';
 import { signIn } from 'next-auth/react';
